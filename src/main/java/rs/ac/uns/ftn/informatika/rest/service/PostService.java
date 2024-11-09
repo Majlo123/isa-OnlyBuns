1ac857a3562e6b59b5c0441b38136e1698685a62
Nikola Andric <nikolandric123@gmail.com>
2024-11-09 01:23:45 +0100

    Restored lost classes

---
+++ src/main/java/rs/ac/uns/ftn/informatika/rest/service/PostService.java
@@ -0,0 +1,57 @@
+package rs.ac.uns.ftn.informatika.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rs.ac.uns.ftn.informatika.rest.domain.Post;
+import rs.ac.uns.ftn.informatika.rest.domain.Comment;
+import rs.ac.uns.ftn.informatika.rest.dto.PostDTO;
+import rs.ac.uns.ftn.informatika.rest.dto.CommentDTO;
+import rs.ac.uns.ftn.informatika.rest.repository.CommentRepository;
+import rs.ac.uns.ftn.informatika.rest.repository.PostRepository;
+import rs.ac.uns.ftn.informatika.rest.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAllByDeletedFalse();
+    }
+
+    public Post getPostById(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
+    }
+
+    public Post createPost(PostDTO postDTO) {
+        Post post = new Post(postDTO.getTitle(),postDTO.getDescription(), postDTO.getImageUrl());
+        return postRepository.save(post);
+    }
+
+    public void deletePost(Long postId) {
+        Post post = getPostById(postId);
+        post.setDeleted(true);
+        postRepository.save(post);
+    }
+
+    public void likePost(Long postId) {
+        Post post = getPostById(postId);
+        post.setLikes(post.getLikes() + 1);
+        postRepository.save(post);
+    }
+
+    public Comment addComment(Long postId, CommentDTO commentDTO) {
+        Post post = getPostById(postId);
+        Comment comment = new Comment(commentDTO.getContent());
+        post.getComments().add(comment);
+        postRepository.save(post);
+        return comment;
+    }
+}
