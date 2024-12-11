579b3f4fae8340aae53cc3921986d6a52c833bf1
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-12-11 01:10:26 +0100

    feat:Follow model added

---
+++ src/main/java/rs/ac/uns/ftn/informatika/rest/domain/Follow.java
@@ -0,0 +1,56 @@
+package rs.ac.uns.ftn.informatika.rest.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "follows", uniqueConstraints = @UniqueConstraint(columnNames = {"follower_id", "followee_id"}))
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "follower_id", nullable = false)
+    private Long followerId;
+
+    @Column(name = "followee_id", nullable = false)
+    private Long followeeId;
+
+    @Column(name = "followed_at", nullable = false)
+    private LocalDateTime followedAt;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFollowerId() {
+        return followerId;
+    }
+
+    public void setFollowerId(Long followerId) {
+        this.followerId = followerId;
+    }
+
+    public Long getFolloweeId() {
+        return followeeId;
+    }
+
+    public void setFolloweeId(Long followeeId) {
+        this.followeeId = followeeId;
+    }
+
+    public LocalDateTime getFollowedAt() {
+        return followedAt;
+    }
+
+    public void setFollowedAt(LocalDateTime followedAt) {
+        this.followedAt = followedAt;
+    }
+}
