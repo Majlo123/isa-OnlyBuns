2760cdded94e62dac5b95874b9fd0492676b58da
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-12-11 01:17:45 +0100

    feat:Follow repository added

---
+++ src/main/java/rs/ac/uns/ftn/informatika/rest/repository/FollowRepository.java
@@ -0,0 +1,17 @@
+package rs.ac.uns.ftn.informatika.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import rs.ac.uns.ftn.informatika.rest.domain.Follow;
+
+import java.time.LocalDateTime;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    boolean existsByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
+
+    @Query("SELECT COUNT(f) FROM Follow f WHERE f.followerId = :followerId AND f.followedAt >= :startTime")
+    long countFollowsInLastMinute(Long followerId, LocalDateTime startTime);
+
+    void deleteByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
+}
