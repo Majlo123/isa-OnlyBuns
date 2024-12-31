b08b6c9fb37e3cacc6fc1463737744b434c04c69
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-12-31 02:05:01 +0100

    small fixes

--- src/main/java/rs/ac/uns/ftn/informatika/rest/domain/Message.java
+++ src/main/java/rs/ac/uns/ftn/informatika/rest/domain/Message.java
@@ -1,5 +1,6 @@
 package rs.ac.uns.ftn.informatika.rest.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
@@ -18,6 +19,7 @@ public class Message {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     @Column(name = "timestamp", nullable = false)
     private LocalDateTime timestamp;
 
